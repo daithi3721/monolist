4b1a98012f28145ff4bf9a5c1c2cea9794aa0724
daithi3721 <m.daithi3721@gmail.com>
2015-12-22 11:02:28 +0000

    monolist-test2-1

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,10 @@ class User < ActiveRecord::Base
 
   has_many :ownerships , foreign_key: "user_id", dependent: :destroy
   has_many :items ,through: :ownerships
-
-
+  has_many :wants, class_name: "Want", foreign_key: "user_id", dependent: :dessroy
+  has_many :want_items ,through: :wants, source: :item
+  has_many :haves, class_name: "Have", foreign_key: "user_id",dependent: :destroy
+  has_many :have_items , through: :heves, source: :item
   # 他のユーザーをフォローする
   def follow(other_user)
     following_relationships.create(followed_id: other_user.id)
@@ -31,20 +33,26 @@ class User < ActiveRecord::Base
 
   ## TODO 実装
   def have(item)
+    haves.create(user_id: item.id)
   end
 
   def unhave(item)
+    haves.find_by(user_id: item.id).destroy
   end
 
   def have?(item)
+    have_items.include?(item)
   end
 
   def want(item)
+    wants.create(user_id: item.id)
   end
 
   def unwant(item)
+    wants.find_by(user_id: item.id).destroy
   end
 
   def want?(item)
+    want_items.include?(item)
   end
 end
